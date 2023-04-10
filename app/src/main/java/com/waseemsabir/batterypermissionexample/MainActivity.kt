5569f8ace6d7643f73ba2046edfa5a923dc9bb9f
Waseem Sabir <waseemsabir2000@gmail.com>
2023-04-10 14:56:23 +0500

    Add Example App and Fix Gradle Version

--- app/src/main/java/com/waseemsabir/batterypermissionexample/MainActivity.kt
+++ app/src/main/java/com/waseemsabir/batterypermissionexample/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.waseemsabir.batterypermissionexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.waseemsabir.batterypermissionexample.databinding.ActivityMainBinding
+import com.waseemsabir.betterypermissionhelper.BatteryPermissionHelper
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+  private lateinit var binding: ActivityMainBinding
+  private val batteryPermissionHelper by lazy { BatteryPermissionHelper() }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    val isBatteryPermissionAvailable =
+        batteryPermissionHelper.isBatterySaverPermissionAvailable(this, true)
+
+    binding.permissionTextView.text =
+        if (isBatteryPermissionAvailable) {
+          "Battery Permission Available for this Device."
+        } else {
+          "No Battery Permission Available for this Device."
+        }
+    binding.permissionTextView.visibility = View.VISIBLE
+    binding.openButton.visibility = if (isBatteryPermissionAvailable) View.VISIBLE else View.GONE
+    binding.openButton.setOnClickListener {
+      batteryPermissionHelper.getPermission(this, open = true, newTask = true)
     }
+  }
 }
